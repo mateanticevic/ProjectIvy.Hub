2d895738a1bb105faa677f6e16da3499c6bee10a
Mate Antičević <Mate.Anticevic@infobip.com>
2025-10-17 22:02:31 +0200

    Refactor Program class structure and update Graylog transport type to UDP

--- ProjectIvy.Hub/Program.cs
+++ ProjectIvy.Hub/Program.cs
@@ -6,34 +6,33 @@ using Serilog.Events;
 using Serilog.Sinks.Graylog;
 using Serilog.Sinks.Graylog.Core.Transport;
 
-namespace ProjectIvy.Hub
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
-                                                  .MinimumLevel.Override(nameof(Microsoft), LogEventLevel.Information)
-                                                  .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                                                  .Enrich.FromLogContext()
-                                                  .WriteTo.Console()
-                                                  .WriteTo.Graylog(new GraylogSinkOptions()
-                                                  {
-                                                      Facility = "project-ivy-hub",
-                                                      HostnameOrAddress = Environment.GetEnvironmentVariable("GRAYLOG_HOST"),
-                                                      Port = Convert.ToInt32(Environment.GetEnvironmentVariable("GRAYLOG_PORT")),
-                                                      TransportType = TransportType.Tcp
-                                                  })
-                                                  .CreateLogger();
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace ProjectIvy.Hub;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-            .UseSerilog();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                                              .MinimumLevel.Override(nameof(Microsoft), LogEventLevel.Information)
+                                              .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                                              .Enrich.FromLogContext()
+                                              .WriteTo.Console()
+                                              .WriteTo.Graylog(new GraylogSinkOptions()
+                                              {
+                                                  Facility = "project-ivy-hub",
+                                                  HostnameOrAddress = Environment.GetEnvironmentVariable("GRAYLOG_HOST"),
+                                                  Port = Convert.ToInt32(Environment.GetEnvironmentVariable("GRAYLOG_PORT")),
+                                                  TransportType = TransportType.Udp
+                                              })
+                                              .CreateLogger();
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+        .UseSerilog();
 }
